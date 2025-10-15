3e8f9f1dc0c8f28b2d24508f3a3321852898c2db
Zerro2003 <mugishaelie160@gmail.com>
2025-10-15 23:48:59 +0200

    Adds multiplication program using while and for loop

---
+++ Java-practice/whileSimple.java
@@ -0,0 +1,11 @@
+public class simpleLoop {
+	public static void main(String[] args) {
+		int i =1;
+        while(i<=10){
+            for(int j=1;j<=10;j++){
+                System.out.print(j + " * " + i + " = " + (j*i) + "\t");
+            }
+            i++;
+        }
+	}
+}
