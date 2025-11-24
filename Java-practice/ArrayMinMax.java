9fd58ebcdac5bb76093ea67f233b3d0677fad498
Zerro2003 <mugishaelie160@gmail.com>
2025-11-24 20:35:49 +0200

    Array min and max

---
+++ Java-practice/ArrayMinMax.java
@@ -0,0 +1,20 @@
+//Question 2
+public class ArrayMinMax {
+    public static void main(String[] args){
+        int[] numbers = {25,14,56,15,36,8,95,42};
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for(int i =1; i< numbers.length;i++){
+            if (numbers[i] > max){
+                max = numbers[i];
+            }
+            if (numbers[i] < min){
+                min = numbers[i];
+            }
+        }
+        System.out.println("Array:" + java.util.Arrays.toString(numbers));
+        System.out.println("Maximum value: " + max);
+        System.out.println("Minimum value: " + min);
+    }
+}
